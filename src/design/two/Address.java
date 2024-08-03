5ad311579c4886f93d56840570aa1e9949880449
Vaibhav Diwan <divaibhav@gmail.com>
2024-08-03 19:16:37 +0530

    created Address.java as per specification

---
+++ src/design/two/Address.java
@@ -0,0 +1,115 @@
+package design.two;
+
+public class Address {
+    private int flatNumber;
+    private String buildingName;
+    private String locality;
+    private String city;
+    private String district;
+    private String state;
+    private String country;
+    private int pinCode;
+
+    public Address(){
+
+    }
+
+    public Address(int flatNumber, String buildingName, String locality, String city, String district, String state, String country, int pinCode) {
+        this.flatNumber = flatNumber;
+        this.buildingName = buildingName;
+        this.locality = locality;
+        this.city = city;
+        this.district = district;
+        this.state = state;
+        this.country = country;
+        this.pinCode = pinCode;
+    }
+
+    public int getFlatNumber() {
+        return flatNumber;
+    }
+
+    public void setFlatNumber(int flatNumber) {
+        this.flatNumber = flatNumber;
+    }
+
+    public String getBuildingName() {
+        return buildingName;
+    }
+
+    public void setBuildingName(String buildingName) {
+        this.buildingName = buildingName;
+    }
+
+    public String getLocality() {
+        return locality;
+    }
+
+    public void setLocality(String locality) {
+        this.locality = locality;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getPinCode() {
+        return pinCode;
+    }
+
+    public void setPinCode(int pinCode) {
+        this.pinCode = pinCode;
+    }
+
+    public String fullAddress(){
+        String response = null;
+        StringBuilder stringBuilder = new StringBuilder();
+        stringBuilder.append(flatNumber);
+        stringBuilder.append(", ");
+        stringBuilder.append(buildingName);
+        stringBuilder.append(", ");
+        stringBuilder.append(locality);
+        stringBuilder.append(", ");
+        stringBuilder.append(city);
+        stringBuilder.append(", ");
+        stringBuilder.append("District - ");
+        stringBuilder.append(district);
+        stringBuilder.append(", ");
+        stringBuilder.append(state);
+        stringBuilder.append(", ");
+        stringBuilder.append(country);
+        stringBuilder.append(", ");
+        stringBuilder.append("Pincode - ");
+        stringBuilder.append(pinCode);
+        response = stringBuilder.toString();
+        return response;
+    }
+}
